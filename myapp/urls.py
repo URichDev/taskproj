eb33bbd8a9436becdab5e47cbd3e9228037375f5
Sergii Anufriiev <sergey@urich.org>
2020-03-20 19:34:53 +0200

    init

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,8 +1,7 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
-from .views import MainView
+from . import views
 
 urlpatterns = [
-    path('', views.MainView.as_view()),
+    path('', views.MainView.as_view())
 ]
